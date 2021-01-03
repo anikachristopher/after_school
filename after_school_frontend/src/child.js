eca1f01dc45b616b30f3b93854d7370a6a4ba161
anikachristopher <anika.mchristoper@gmail.com>
2021-01-03 07:17:55 -0500

    built methods in individual class folders

--- after_school_frontend/src/child.js
+++ after_school_frontend/src/child.js
@@ -3,6 +3,21 @@ class Child{
         this.name = name;
         this.age = age;
         this.grade = grade
+        //whatever behaviour belongs to this class will be stored here. Will have a render child method.
+        //should my constructor include an id attribute
+        //cretae a render child method to show the child and associate with an ID
     }
+
+    // render newChild()
+
+
+    //     fetch(`${BASE_URL}/children`)
+    //     .then(resp => resp.json())
+    //     .then(children => {
+    //         for (const child of children){
+    //             console.log(child)
+    //             let c = new Child(child.name, child.age, child.grade)
+    //     })
+    // }
 }
-//whatever behaviour belongs to this clas will be stored here. Will have a render child method.
+
--- after_school_frontend/src/schedule.js
+++ after_school_frontend/src/schedule.js
@@ -6,4 +6,15 @@ class Schedule{
         this.content = content;
         this.child_id = child_id
     }
+
+    renderSchedules() {
+        let schedulesDiv = document.getElementById("schedules-container");
+        let currentWeekday = this.weekday
+        const weekday = currentWeekday;
+    }
 }
+
+//build a render schedule method 
+//include this in the DOM content loaded to fire at the start of the app
+//build the create schedule button and attach event listener to fire off the new schedule variable defined within
+//ensure that child_id is associated to all for child to create schedules
