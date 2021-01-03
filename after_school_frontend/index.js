ff340781d53bc3bb6d2517c72b33f49b5f624cef
anikachristopher <anika.mchristoper@gmail.com>
2021-01-03 07:17:12 -0500

    built function to fetch all schedules

--- after_school_frontend/index.js
+++ after_school_frontend/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    debugger;
-})
+    //debugger;
+    fetchSchedules();
+});
+
+const BASE_URL = "http://127.0.0.1:3000/";
+
+//build function using fetch request to display any existing schedules - READ - done
+//include this function in the DOM content loaded to fire upon app start - done
+//build a create schedule fetch request - CREATE
+//define a new schedule variable and include within the fetch request - create fetch
+//build a delete fetch request here - DELETE
+
+
+function fetchSchedules() {
+    fetch(`${BASE_URL}/schedules`)
+    .then((resp) => resp.json())
+    .then((schedules) => {
+        for (const schedule of schedules){
+            // console.log("rails obj", schedule)
+            let newScheduleCard = new Schedule(
+                schedule.id,
+                schedule.weekday,
+                schedule.subject,
+                schedule.content,
+                schedule.child_id
+            )
+            // console.log("js obj", newScheduleCard);
+            //newScheduleCard.renderSchedule();
+            //renderSchedule method not built as yet
+        };
+    });
+}
+
+function createSchedule() {}
+
+function deleteSchedule() {}
