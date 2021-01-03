e1052f24e9b916488f91b4e444a8aba42386fb5f
anikachristopher <anika.mchristoper@gmail.com>
2021-01-02 22:27:41 -0500

    seeded the database

--- after_school_backend/db/seeds.rb
+++ after_school_backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+malik = Child.create(name: "Malik", age: "9", grade: "fourth")
+
+malik.schedules.create([
+    {weekday: "Monday", subject: "Math", content: "Completed chapter 1 of Go Math"},
+    {weekday: "Tuesday", subject: "Reading", content: "Completed chapter 1 of Lost"},
+])
