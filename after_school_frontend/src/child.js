e14e20ec9a01d55a29dd69fabdafd449d24a2896
anikachristopher <anika.mchristoper@gmail.com>
2021-01-03 04:53:32 -0500

    setup classes and their constructors

---
+++ after_school_frontend/src/child.js
@@ -0,0 +1,8 @@
+class Child{
+    constructor(name, age, grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade
+    }
+}
+//whatever behaviour belongs to this clas will be stored here. Will have a render child method.
---
+++ after_school_frontend/src/schedule.js
@@ -0,0 +1,9 @@
+class Schedule{
+    constructor(id, weekday, subject, content, child_id){
+        this.id = id;
+        this.weekday = weekday;
+        this.subject = subject;
+        this.content = content;
+        this.child_id = child_id
+    }
+}
