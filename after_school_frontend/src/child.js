77fce4d05a7409c6fc3095e0766ebce60a6f59cf
anikachristopher <anika.mchristoper@gmail.com>
2021-01-03 22:18:48 -0500

    update child class with constructors

--- after_school_frontend/src/child.js
+++ after_school_frontend/src/child.js
@@ -1,23 +1,28 @@
 class Child{
-    constructor(name, age, grade){
+    constructor(id, name, age, grade){
+        this.id = id;
         this.name = name;
         this.age = age;
         this.grade = grade
         //whatever behaviour belongs to this class will be stored here. Will have a render child method.
         //should my constructor include an id attribute
-        //cretae a render child method to show the child and associate with an ID
+        //create a render child method to show the child and associate with an ID
     }
 
-    // render newChild()
+ 
 
 
-    //     fetch(`${BASE_URL}/children`)
-    //     .then(resp => resp.json())
-    //     .then(children => {
-    //         for (const child of children){
-    //             console.log(child)
-    //             let c = new Child(child.name, child.age, child.grade)
-    //     })
-    // }
+
 }
 
+
+
+
+
+
+
+
+
+
+//make a child container
+// set as a variable and do innerHTML
