701ab9ab708896ba3ff81a451a8c43d4979893dc
anikachristopher <anika.mchristoper@gmail.com>
2021-01-03 22:21:36 -0500

    create render schedule class method

--- after_school_frontend/src/schedule.js
+++ after_school_frontend/src/schedule.js
@@ -8,13 +8,45 @@ class Schedule{
     }
 
     renderSchedules() {
-        let schedulesDiv = document.getElementById("schedules-container");
-        let currentWeekday = this.weekday
-        const weekday = currentWeekday;
+        
+        // let scheduleForm = document.getElementById("forms-container");
+        // scheduleForm.remove(); 
+    //If it isn't "undefined" and it isn't "null", then it exists.
+        // if(typeof(scheduleForm) != 'undefined' && scheduleForm != null){
+        // scheduleForm.remove()} 
+        let allSchedules = document.getElementById("schedules-container");
+        
+       
+        // let schedulesDiv = document.getElementById("schedules-container");
+        // debugger;
+        // fetch 
+        allSchedules.innerHTML += 
+        // let currentWeekday = this.weekday
+        // const weekday = currentWeekday;
+            `<div id=${this.id} name="schedule-card">
+            <h4>Weekday: ${this.weekday}</h4>
+            <h4>Subject: ${this.subject}</h4>
+            <h4>Content: ${this.content}</h4>
+            <br>
+            <button class="delete-schedule" type="delete">Delete</button>
+            </div>`
+            
+
+            deleteButton()
     }
+
 }
+
+
+
+
+
+
+
+
 
 //build a render schedule method 
 //include this in the DOM content loaded to fire at the start of the app
 //build the create schedule button and attach event listener to fire off the new schedule variable defined within
 //ensure that child_id is associated to all for child to create schedules
+
