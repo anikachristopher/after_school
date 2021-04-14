99e8716ceda33d036107991967fe3ecc798b58dc
Anika Christopher <anika.mchristopher@gmail.com>
2021-04-14 00:01:58 -0400

    removed innerHTML from schedule class

--- after_school_frontend/src/schedule.js
+++ after_school_frontend/src/schedule.js
@@ -36,83 +36,66 @@ class Schedule{
     }
 
 }
-function deleteButton() {
-    // let createDeleteButton = document.createElement("button");
-    // createDeleteButton.className = ("delete-schedule-button")
-    // createDeleteButton.classList.add("delete")
-    // // createDeleteButton.innerHTML = "Delete"
-    // let scheduleCards = document.getElementsByClassName("delete-schedule")
-    // debugger;
-    // scheduleCards.append(createDeleteButton)
+    const modal = document.getElementById("myModal");
 
-    //let deleteButtonListener = document.getElementById("delete-button");
-    document.querySelectorAll('.delete-schedule').forEach(button => {
-        button.addEventListener('click', event => {
-          deleteSchedule(event)
-        })
-      })
-    // createDeleteButton.addEventListener("click", e => deleteSchedule(e));
-}
+// Get the button that opens the modal
+    const btn = document.getElementById("myBtn");
 
-function addSubmitButton(){
-    let createScheduleButton = document.createElement("button");
-    createScheduleButton.className = ("new-schedule-button")
-    createScheduleButton.classList.add("submit")
-    createScheduleButton.innerHTML = "Submit"
+// Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+
+
+// function deleteButton() {
+//     // let createDeleteButton = document.createElement("button");
+//     // createDeleteButton.className = ("delete-schedule-button")
+//     // createDeleteButton.classList.add("delete")
+//     // // createDeleteButton.innerHTML = "Delete"
+//     // let scheduleCards = document.getElementsByClassName("delete-schedule")
+//     // debugger;
+//     // scheduleCards.append(createDeleteButton)
+
+//     //let deleteButtonListener = document.getElementById("delete-button");
+//     document.querySelectorAll('.delete-schedule').forEach(button => {
+//         button.addEventListener('click', event => {
+//           deleteSchedule(event)
+//         })
+//       })
+//     // createDeleteButton.addEventListener("click", e => deleteSchedule(e));
+// }
+
+// function addSubmitButton(){
+//     // let createScheduleButton = document.createElement("button");
+//     // createScheduleButton.className = ("new-schedule-button")
+//     // createScheduleButton.classList.add("submit")
+//     // createScheduleButton.innerHTML = "Submit"
 
     
-    let newScheduleForm = document.getElementById("new-schedule-form");
-    // debugger;
-    newScheduleForm.appendChild(createScheduleButton)
-    newScheduleForm.addEventListener("submit", e => createSchedule(e));
-            newScheduleForm.reset();
+//     let newScheduleForm = document.getElementById("new-schedule-form");
+//     // debugger;
+//     // newScheduleForm.appendChild(createScheduleButton)
+//     newScheduleForm.addEventListener("submit", e => createSchedule(e));
+//             newScheduleForm.reset();
             
-    // scheduleForm.remove()
+//     // scheduleForm.remove()
 
     
-}
-
-
-function showScheduleForm(){
-    let formDiv = document.getElementById("schedules-container");
-    // while (allSchedules.firstChild) {
-    //     allSchedules.removeChild(allSchedules.firstChild);
-    //   }
-    
-// debugger;
-    formDiv.innerHTML += `
-    <form id="new-schedule-form">
-        <input type="hidden" id="child_id" name="child_id" value=${event.target.previousSibling.id}>
-          <label for="weekday_id">Weekday:</label><br>
-          <select id="weekday_id" name="weekday">
-              <option value=""></option>
-              <option value="Monday">Monday</option>
-              <option value="Tuesday">Tuesday</option>
-              <option value="Wednesday">Wednesday</option>
-              <option value="Thursday">Thursday</option>
-              <option value="Friday">Friday</option>
-          </select>
-          <label for="subject_id">Subject:</label><br>
-          <select id="subject_id" name="subject">
-              <option value=""></option>
-              <option value="Math">Math</option>
-              <option value="Reading">Reading</option>
-              <option value="Creative Writing">Creative Writing</option>
-          </select>
-         <br>
-         <label>Content:</label>
-         <textarea name="content" id="content" cols="30" rows="10" required></textarea>
-          </br>
-          <div id="submit-button-div"></div>
-
-        </form> `
-          addSubmitButton();
-         
-         
-          
-}
-
-
+// }
 
 
 
@@ -124,3 +107,4 @@ function showScheduleForm(){
 //build the create schedule button and attach event listener to fire off the new schedule variable defined within
 //ensure that child_id is associated to all for child to create schedules
 
+    }
